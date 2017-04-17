c84461cedd7e80792be3bf6c9af134be336ead4c
Zachary Burnett <zrb@umd.edu>
2017-04-17 13:40:28 -0400

    changed to indexing for loop to preserve order of desired fields

--- parseAPRS.py
+++ parseAPRS.py
@@ -8,42 +8,46 @@ output_filename = "sample_packets/KG5RHL-11_parsed.txt"
 all_fields = ['from', 'via', 'path', 'to', 'latitude', 'longitude', 'altitude', 'posambiguity', 'course', 'speed', 'format', 'messagecapable', 'symbol', 'symboltable', 'raw', 'comment']
 
 # define which fields will be written into the output file
-desired_fields = {'latitude', 'longitude', 'altitude', 'from', 'speed', 'course', 'comment'}
+desired_fields = ['latitude', 'longitude', 'altitude', 'speed', 'course', 'from', 'comment']
 
-# define string to use in case of null data (field does not exist in packet)
+# define string to use in case of null data (if a certain field does not exist in a packet)
 null_data_value = "NA"
 
 # read all lines of input file
 with open(input_filename, "r") as input_file:
-    input_lines = input_file.readlines()
+    aprs_packets = input_file.readlines()
 
 # create array to hold timestamps
 timestamps = []
 
-# populate timestamps
-for row in range(0, len(input_lines)):
-    current_line = input_lines[row].split(': ')
+# populate timestamps with first value in every line (ended by a colon) before actual APRS packet
+for packet_index in range(0, len(aprs_packets)):
+    current_line = aprs_packets[packet_index].split(': ')
     timestamps.append(current_line[0])
-    input_lines[row] = current_line[1]
+    aprs_packets[packet_index] = current_line[1]
 
 # open output and write header, then write parsed APRS data
 with open(output_filename, "w") as output_file:
     output_file.write('timestamp,' + ','.join(desired_fields) + '\n')
     
-    # for each row in the input data, parse the APRS data using aprslib and extract the desired fields
-    for row in range(0, len(input_lines)):     
+    # for each line in the input data, parse the APRS data using aprslib and extract the desired fields
+    for packet_index in range(0, len(aprs_packets)):     
         # parse data and append timestamp to front   
-        current_line_all_fields = aprslib.parse(input_lines[row])
-        current_line_desired_fields = [str(timestamps[row])]
+        current_aprs_packet = aprslib.parse(aprs_packets[packet_index])
+        current_output_data = [str(timestamps[packet_index])]
 
-        # extract and append desired fields, preservign CSV format
-        for field in desired_fields:
-            if field in current_line_all_fields:
+        # extract and append desired fields, preserving CSV format by replacing commas with semicolons
+        for field_index in range(0, len(desired_fields)):
+            # get current field name
+            field = desired_fields[field_index]
+            
+            # check if field exists in current packet
+            if field in current_aprs_packet:
                 # replace commas in data with semicolons to preserve CSV format
-                current_line_desired_fields.append(str(current_line_all_fields[field]).replace(',', ';'))
+                current_output_data.append(str(current_aprs_packet[field]).replace(',', ';'))
             else:
                 # if the field is not in the current APRS packet, append the null data value instead
-                current_line_desired_fields.append(null_data_value)
+                current_output_data.append(null_data_value)
         
         # write line to output file
-        output_file.write(','.join(current_line_desired_fields) + '\n')
+        output_file.write(','.join(current_output_data) + '\n')
